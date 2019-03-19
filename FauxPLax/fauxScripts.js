e851d57e2298123103b0be1d839c24f79fedfd8c
Sam Rasmussen <samrandomonium@gmail.com>
2019-03-18 21:34:34 -0400

    Let's try this

--- FauxPLax/fauxScripts.js
+++ FauxPLax/fauxScripts.js
@@ -1,32 +1,35 @@
-$(document).ready(function() {
-	
 	var width = $(window).width();
 	var height = $(window).height();
-	var botBarPos = height - 50;
-	var boxVert = height/2-25;
-	var boxHori = width/2-25;
+	var botBarPos = height - 300;
+	var movingBoxVert = height/2-25;
+	var movingBoxHori = width/2-25;
 
 	// alert(width + ", " + height)
 
-	$("body").css("maxWidth", width)
-	$("body").css("maxHeight", height)
+	$("#contentContain").css("maxWidth", width)
+	$("#contentContain").css("maxHeight", height)
 
 	$("#bottomStripe").css("top", botBarPos)
 
-	$("#movingBox").css("top", boxVert)
-	$("#movingBox").css("left", boxHori)
+	$("#movingBox").css("top", movingBoxVert)
+	$("#movingBox").css("left", movingBoxHori)
 
-});
+var backG =   document.querySelector('#background')
+var box 	=   document.querySelector('#movingBox')
 
-var backG 	= document.querySelector('#background')
-var box 	= document.querySelector('#movingBox')
+// var maxX =    garden.clientWidth  - ball.clientWidth;
+// var maxY =    garden.clientHeight - ball.clientHeight;
 
 function handleOrientation(event) {
+
+  var x = event.beta;  // In degree in the range [-180,180]
+  var y = event.gamma; // In degree in the range [-90,90]
+
   var beta     = event.beta;
   var gamma    = event.gamma;
 
-  output.innerHTML  = "beta : " + x + "\n";
-  output.innerHTML += "gamma: " + y + "\n";
+  // output.innerHTML  = "beta : " + x + "\n";
+  // output.innerHTML += "gamma: " + y + "\n";
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
@@ -40,8 +43,8 @@ function handleOrientation(event) {
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  box.style.top  = (maxX*x/90 - 25) + "px";
-  box.style.left = (maxY*y/90 - 25) + "px";
+  box.style.top  = (width*x/90 - 25) + "px";
+  box.style.left = (height*y/90 - 25) + "px";
 
 }
 
