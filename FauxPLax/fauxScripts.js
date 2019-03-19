d46a41154a3774f0d1709ef105ea1e5f4b70e341
Sam Rasmussen <samrandomonium@gmail.com>
2019-03-18 22:53:29 -0400

    Ok I think I fixed it
    I think

--- FauxPLax/fauxScripts.js
+++ FauxPLax/fauxScripts.js
@@ -1,8 +1,8 @@
 	var width = $(window).width();
 	var height = $(window).height();
 	var botBarPos = height - 600;
-	// var movingBoxVert = height/2-25;
-	// var movingBoxHori = width/2-25;
+	var movingBoxVert = height/2-25;
+	var movingBoxHori = width/2-25;
 
 	// alert(width + ", " + height)
 
@@ -11,8 +11,8 @@
 
 	$("#bottomStripe").css("top", botBarPos)
 
-	// $("#movingBox").css("top", movingBoxVert)
-	// $("#movingBox").css("left", movingBoxHori)
+	$("#movingBox").css("top", movingBoxVert)
+	$("#movingBox").css("left", movingBoxHori)
 
 var backG =   document.querySelector('#background')
 var box 	=   document.querySelector('#movingBox')
@@ -24,6 +24,8 @@ function handleOrientation(event) {
 
   var x = event.beta;  // In degree in the range [-180,180]
   var y = event.gamma; // In degree in the range [-90,90]
+  var imgH = ($("#movingBox").height())
+  var imgV = ($("#movingBox").width())
 
   var beta     = event.beta;
   var gamma    = event.gamma;
@@ -43,8 +45,8 @@ function handleOrientation(event) {
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  box.style.top  = (height*x/1080 - 25) + "px";
-  box.style.left = (width*y/1080 - 25) + "px";
+  box.style.top  = (height*x/90 - imgH) + "px";
+  box.style.left = (width*y/90 - imgV) + "px";
 
 }
 
