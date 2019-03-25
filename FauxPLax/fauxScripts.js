0d58726c844415e2decb76b736d0d317fc449282
Sam Rasmussen <samrandomonium@gmail.com>
2019-03-25 13:39:46 -0400

    0.32.13
    Added text box, more styling incoming

--- FauxPLax/fauxScripts.js
+++ FauxPLax/fauxScripts.js
@@ -2,19 +2,12 @@
 	var height = $(window).height();
 	var botBarPos = height - 600;
   var tiltAmp = 460;
-  // var movingBoxVert = height/2-25;
-  // var movingBoxHori = width/2-25;
 
 	$("#contentContain").css("maxWidth", width)
 	$("#contentContain").css("maxHeight", height)
 
-	// $("#bottomStripe").css("top", botBarPos)
-
-	// $("#movingBox").css("top", movingBoxVert)
-	// $("#movingBox").css("left", movingBoxHori)
-
-var backG =   document.querySelector('#background img')
-var box 	=   document.querySelector('#movingBox')
+var backG =   document.querySelector('#bgImage')
+var box 	=   document.querySelector('#character')
 
 //Handles character motion
 function handleOrientation(event) {
@@ -22,8 +15,8 @@ function handleOrientation(event) {
   var x     = event.beta;
   var y     = event.gamma;
 
-  var imgH  = ($("#movingBox").height());
-  var imgV  = ($("#movingBox").width());
+  var imgH  = ($("#character").height());
+  var imgV  = ($("#character").width());
   var bV    = ($("body").height());
   var bH    = ($("body").width())
 
@@ -41,8 +34,8 @@ function handleOrientation(event) {
   // Centering
   box.style.top     = (height*x/tiltAmp - imgH-100) + "px";
   box.style.left    = (width*y/tiltAmp - imgV) + "px";
-  backG.style.top   = (height*x/tiltAmp - bV) + "px";
-  backG.style.left  = (width*y/tiltAmp - bH) + "px";
+  backG.style.top   = (height*x/(tiltAmp*5) - bV) + "px";
+  backG.style.left  = (width*y/(tiltAmp*5) - bH) + "px";
 
 }
 
