16652466ad01ac9aa93544d46aa5b77051f1027a
Sam Rasmussen <samrandomonium@gmail.com>
2019-04-13 22:41:17 -0400

    0.9
    One final effort

--- FauxPLax/fauxScripts.js
+++ FauxPLax/fauxScripts.js
@@ -8,11 +8,12 @@ for (i = 0; i < 13; ++i) {
 
 
 // Get width and height of device window, and height of text container
-  var windowWidth     = $(window).width();
-  var windowHeight    = $(window).height();
-  var boxOffset       = windowHeight - 420 - 180;
+  var windowWidth     = $(window).innerWidth();
+  var windowHeight    = $(window).innerHeight();
+  var boxOffset       = windowHeight - 420 - 170;
   var tableWidth      = $("#thisIsTable")
   
   $(".textContainer").css("marginTop", boxOffset);
   $("body").css("maxHeight", windowHeight);
   // $(".textContainer").css("maxWidth", windowWidth);
+  console.log($("body").css("maxHeight"))
