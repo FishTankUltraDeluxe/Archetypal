8c37146d885fcb2a4d3dac4e17f410c6ebbc50da
Sam Rasmussen <samrandomonium@gmail.com>
2019-04-17 14:14:08 -0400

    More restructuring
    Some necessary updates to enhance scalability

---
+++ FauxPLax/fauxScripts2.js
@@ -0,0 +1,22 @@
+
+
+// Set up parallax for each table element
+var i;
+
+for (i = 0; i < 13; ++i) {
+  var scene = $('.bodyContain').get(i);
+  var parallaxInstance = new Parallax(scene);
+}
+
+
+// // Get width and height of device window, and height of text container
+//   var windowWidth     = $(window).innerWidth();
+//   var windowHeight    = $(window).innerHeight();
+//   var boxOffset       = windowHeight - 300 - 120;
+//   var tableWidth      = $("#thisIsTable")
+  
+//   $(".textContainer").css("marginTop", boxOffset);
+//   $("body").css("maxHeight", windowHeight);
+//   $(".bodyContain").css("maxHeight", windowHeight);
+  // $(".textContainer").css("maxWidth", windowWidth);
+  //console.log($("body").css("maxHeight"));
